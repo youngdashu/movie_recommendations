ca4930a83703e1b693a10b25c9acc3a2a06fc58a
youngdashu <anowak2000@gmail.com>
2023-04-03 01:32:39 +0200

    added files

---
+++ docker-entrypoint-initdb.d/init-database.sh
@@ -0,0 +1,9 @@
+apt-get update
+apt-get install -y python3
+apt-get install -y git
+apt-get install -y wget
+
+git clone https://github.com/youngdashu/movie_recommendations.git
+git checkout finish_parsers
+
+ls
