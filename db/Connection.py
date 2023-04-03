11a8dd62c97ee66549d98aa567af1024735b2901
youngdashu <anowak2000@gmail.com>
2023-04-03 04:23:05 +0200

    changed name

--- db/Connection.py
+++ db/Connection.py
@@ -4,4 +4,4 @@ from sqlalchemy import create_engine
 
 class Connection:
     def __init__(self):
-        self.engine = create_engine("postgresql+psycopg2://postgres:postgrespw@localhost:5440/docker", echo=False)
+        self.engine = create_engine("postgresql+psycopg2://postgres:postgrespw@localhost:5440/postgres", echo=False)
