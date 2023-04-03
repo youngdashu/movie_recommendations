9b853e5eabf5b90cf928368175366cf4e8384ba2
youngdashu <anowak2000@gmail.com>
2023-04-03 04:02:49 +0200

    main changes

--- docker_start.py
+++ docker_start.py
@@ -1,3 +1,47 @@
+from sqlalchemy import delete, insert
+from sqlalchemy.orm import Session
+
+from db import Connection
+from models.Base import Base
+from models.Movie import Genre
+
 if __name__ == '__main__':
-    from db.seed import main
-    main()
+    connection = Connection()
+    Base.metadata.drop_all(connection.engine)
+    Base.metadata.create_all(connection.engine)
+    with Session(connection.engine) as session:
+        session.execute(delete(Genre))
+
+    with Session(connection.engine) as session:
+        genre_names = [
+            "Action",
+            "Adventure",
+            "Animation",
+            "Children",
+            "Comedy",
+            "Crime",
+            "Documentary",
+            "Drama",
+            "Fantasy",
+            "Film-Noir",
+            "Horror",
+            "Musical",
+            "Mystery",
+            "Romance",
+            "Sci-Fi",
+            "Thriller",
+            "War",
+            "Western",
+        ]
+
+        special_genres = ['IMAX']
+
+        session.execute(
+            insert(Genre),
+            list(
+                map(lambda name: {"name": name}, genre_names + special_genres)
+            )
+
+        )
+
+        session.commit()
